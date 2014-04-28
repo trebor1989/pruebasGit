b17288246c756a50eff0d07391ec476810763876
Nocturno <Nocturno@02-C7>
2014-04-28 16:51:01 +0200

    Primer commit proyectoRoberto con ClaseRoberto

---
+++ src/paqueteRoberto/ClaseRoberto.java
@@ -0,0 +1,14 @@
+package paqueteRoberto;
+
+/**
+ *
+ * @author Roberto
+ *
+ */
+public class ClaseRoberto {
+
+    public static void main(String[] args) {
+        javax.swing.JOptionPane.showMessageDialog(null, "Hola Mundo");
+    }
+    
+}
