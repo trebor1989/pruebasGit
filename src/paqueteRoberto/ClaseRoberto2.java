990789f487ec28cdd5bd4ead25750481b7d1bbda
Nocturno <Nocturno@02-C7>
2014-04-28 17:59:05 +0200

    commit ClaseRoberto3

---
+++ src/paqueteRoberto/ClaseRoberto2.java
@@ -0,0 +1,12 @@
+package paqueteRoberto;
+
+/**
+ *
+ * @author Roberto
+ *
+ */
+public class ClaseRoberto2 {
+    public static void main(String[] args) {
+        javax.swing.JOptionPane.showMessageDialog(null, "Hola Mundo 2");
+    }
+}
