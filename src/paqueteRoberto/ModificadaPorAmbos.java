532c25501f70dad54037acbc204db82154bd707a
Nocturno <Nocturno@02-C7>
2014-05-05 16:43:35 +0200

    commit ClaseRoberto3

---
+++ src/paqueteRoberto/ModificadaPorAmbos.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package paqueteRoberto;
+
+/**
+ *
+ * @author Nocturno
+ *
+ *
+ *
+ */
+public class ModificadaPorAmbos {
+    public static void main(String[] args) {
+        javax.swing.JOptionPane.showMessageDialog(null, "Hola Mundo modificada por ambos");
+    }
+    
+    private void metodoRoberto(){
+        
+        for (int i = 0; i < 10; i++) {
+            javax.swing.JOptionPane.showMessageDialog(null, "Nº: " + i);
+        }
+        
+    }
+}
